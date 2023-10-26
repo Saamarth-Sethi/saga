7b267ce1b2d67a8bdc62a04b6ea07367c21de1c5
Saamarth-Sethi <saamarthsethi@gmail.com>
2023-10-26 15:09:06 -0700

    fixed range sufferage algorithm

--- saga/common/range_suffrage.py
+++ saga/common/range_suffrage.py
@@ -1,88 +1,77 @@
-import logging
-import pathlib
-from typing import Dict, Hashable, List, Tuple
-
+from functools import lru_cache
+from typing import Dict, Hashable, List
 import networkx as nx
-import numpy as np
 
-from ..base import Task
-from ..base import Scheduler
-from ..utils.tools import check_instance_simple, get_insert_loc
+from ..scheduler import Scheduler, Task
 
-thisdir = pathlib.Path(__file__).resolve().parent
+class SufferageScheduler(Scheduler):
+    """Sufferage scheduler"""
+    def schedule(self, network: nx.Graph, task_graph: nx.DiGraph) -> Dict[Hashable, List[Task]]:
+        """Schedules the task graph on the network
 
-def range_suffrage_sort(network: nx.Graph, task_graph: nx.DiGraph) -> List[Hashable]:
-    """Sort tasks based on the range suffrage model.
+        Args:
+            network (nx.Graph): The network.
+            task_graph (nx.DiGraph): The task graph.
 
-    Args:
-        network (nx.Graph): The network graph.
-        task_graph (nx.DiGraph): The task graph.
+        Returns:
+            Dict[Hashable, List[Task]]: The schedule.
+        """
+        schedule: Dict[Hashable, List[Task]] = {}
+        scheduled_tasks: Dict[Hashable, Task] = {}  # Map from task_name to Task
 
-    Returns:
-        List[Hashable]: The sorted list of tasks.
-    """
-    task_ranges = {}
-    for task_name in task_graph.nodes:
-        start_time = min(task_graph.nodes[task_name]['start_times'])
-        end_time = max(task_graph.nodes[task_name]['end_times'])
-        task_ranges[task_name] = end_time - start_time
+        def get_eet(task: Hashable, node: Hashable) -> float:
+            return task_graph.nodes[task]['weight'] / network.nodes[node]['weight']
 
-    return sorted(list(task_ranges.keys()), key=task_ranges.get, reverse=True)
+        def get_commtime(task1: Hashable, task2: Hashable, node1: Hashable, node2: Hashable) -> float:
+            return task_graph.edges[task1, task2]['weight'] / network.edges[node1, node2]['weight']
 
+        def get_eat(node: Hashable) -> float:
+            return schedule[node][-1].end if schedule.get(node) else 0
 
-class RangeSuffrageScheduler(Scheduler):
-    def __init__(self):
-        super().__init__()
-
-    @staticmethod
-    def get_runtimes(network: nx.Graph, task_graph: nx.DiGraph) -> Dict[Hashable, Dict[Hashable, float]]:
-        runtimes = {}
-        for node in network.nodes:
-            runtimes[node] = {}
-            speed: float = network.nodes[node]["weight"]
-            for task in task_graph.nodes:
-                cost: float = task_graph.nodes[task]["weight"]
-                runtimes[node][task] = cost / speed
-                logging.debug(f"Task {task} on node {node} has runtime {runtimes[node][task]}")
-        return runtimes
-
-    def _schedule(self, 
-                  network: nx.Graph, 
-                  task_graph: nx.DiGraph,
-                  runtimes: Dict[Hashable, Dict[Hashable, float]],
-                  schedule_order: List[Hashable]) -> Dict[Hashable, List[Task]]:
-        comp_schedule: Dict[Hashable, List[Task]] = {node: [] for node in network.nodes}
-        task_schedule: Dict[Hashable, Task] = {}
-
-        for task_name in schedule_order:
-            min_finish_time = np.inf 
-            best_node = None 
-            for node in network.nodes:
-                logging.debug(f"Testing task {task_name} on node {node}")
-                max_arrival_time: float = max([
-                    0.0, *[
-                        task_schedule[parent].end
-                        for parent in task_graph.predecessors(task_name)
-                    ]
-                ])
-
-                runtime = runtimes[node][task_name]   
-                idx, start_time = get_insert_loc(comp_schedule[node], max_arrival_time, runtime)
-                
-                finish_time = start_time + runtime
-                if finish_time < min_finish_time:
-                    min_finish_time = finish_time
-                    best_node = node, idx 
+        def get_fat(task: Hashable, node: Hashable) -> float:
+            return 0 if task_graph.in_degree(task) <= 0 else max([
+                scheduled_tasks[pred_task].end +
+                get_commtime(pred_task, task, scheduled_tasks[pred_task].node, node)
+                for pred_task in task_graph.predecessors(task)
+            ])
             
-            new_runtime = runtimes[best_node[0]][task_name]
-            task = Task(best_node[0], task_name, min_finish_time - new_runtime, min_finish_time)
-            comp_schedule[best_node[0]].insert(best_node[1], task)
-            task_schedule[task_name] = task
+        def get_ect(task: Hashable, node: Hashable) -> float:
+            return get_eet(task, node) + max(get_eat(node), get_fat(task, node))
 
-        return comp_schedule
-    
-    def schedule(self, network: nx.Graph, task_graph: nx.DiGraph) -> Dict[str, List[Task]]:
-        check_instance_simple(network, task_graph)
-        runtimes = RangeSuffrageScheduler.get_runtimes(network, task_graph)
-        schedule_order = range_suffrage_sort(network, task_graph)  # Use  range suffrage sorting here
-        return self._schedule(network, task_graph, runtimes, schedule_order)
+        def clear_caches():
+            """Clear all caches."""
+            get_eat.cache_clear()
+            get_fat.cache_clear()
+            get_ect.cache_clear()
+
+        while len(scheduled_tasks) < task_graph.order():
+            available_tasks = [
+                task for task in task_graph.nodes
+                if task not in scheduled_tasks and all(pred in scheduled_tasks
+                                                       for pred in task_graph.predecessors(task))
+            ]
+
+            sufferages = {}
+            for task in available_tasks:
+                ect_values = [get_ect(task, node) for node in network.nodes]
+                first_ect = min(ect_values)
+                ect_values.remove first_ect
+                second_ect = min(ect_values) if ect_values else first_ect
+                sufferages[task] = second_ect - first_ect
+
+            sched_task = max(sufferages, key=sufferages.get)
+            sched_node = min(network.nodes, key=lambda node: get_ect(sched_task, node))
+
+            schedule.setdefault(sched_node, [])
+            new_task = Task(
+                node=sched_node,
+                name=sched_task,
+                start=max(get_eat(sched_node), get_fat(sched_task, sched_node)),
+                end=get_ect(sched_task, sched_node)
+            )
+            schedule[sched_node].append(new_task)
+            scheduled_tasks[sched_task] = new_task
+
+            clear_caches()
+
+        return schedule
